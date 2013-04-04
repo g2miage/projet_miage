736e4d5ae85ef55abe123e8d8f126691bbf813c2
Julien Monchany <julien.monchany@gmail.com>
2013-04-04 17:10:37 +0200

    Revert "test accès en ecriture Signed-off-by:G2.miage <g2.miage@gmail.com>"
    This reverts commit 83eb1a9b3beb4fdd664c0dfddf9247628a0df6bc.

--- app/Config/core.php
+++ app/Config/core.php
@@ -200,12 +200,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '02014f15e07a696427131853b7be129b2a34dcc3');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '565956826323020678830292215057');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
