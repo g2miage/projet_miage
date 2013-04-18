b0faeffc800e12ff719776ce43a0f4f36e1f1a5f
Mikel <Mikel@10.3.2.223>
2013-04-18 17:15:47 +0200

    Ajout du captcha lors de l'inscription de l'utilisateur

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -31,6 +31,7 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+        Router::connect('/img/captcha.jpg', array('controller' => 'users', 'action' => 'captcha'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
